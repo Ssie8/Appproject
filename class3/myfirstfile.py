bbb10c1b74e951e17a6e86a1a27a0fc222a46a83
Ssie8 <101572404+Ssie8@users.noreply.github.com>
2022-03-21 16:56:53 +0100

    Update myfirstfile.py

--- class3/myfirstfile.py
+++ class3/myfirstfile.py
@@ -8,9 +8,17 @@ if genre == 'Comedy':
 else:
      st.write("You didn't select comedy.")
      
+st.write('weather',
 import json, requests  
+        
 APIkey = 319af15e7367c7b87e592b7c6d84197e
-location = 'london'
+import streamlit as st
+location = st.radio("what's the weather in"),('London', 'Madrid', 'Sydney'))
+st.write('the weather for today')
+st.write('the temperature of' location)
 url = 'http://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=' + APIkey + &units=metric
 response = request.get(url)
-weatherData = json.loads(respond.text)
+weatherData = json.loads(response.text)
+st.write('max temperature', weatherData [] )
+
+print(weatherData['main']['temp_max'])
